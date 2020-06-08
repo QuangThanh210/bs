952420f14b8249f8929aee91803f2122ef527ed1
topganhteam2@gmail.com <topganhteam2@gmail.com>
2020-06-08 16:10:14 +0700

    bigschool

---
+++ bigschool/Controllers/CoursesController.cs
@@ -0,0 +1,30 @@
+﻿using bigschool.Models;
+using bigschool.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bigschool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+            public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+
+        }
+
+        // GET: Courses
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
